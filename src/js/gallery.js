78bc907d7b72109c032f7ec15025d31a45c4184d
Sergii Antonenko <gerbar50@gmail.com>
2025-01-17 00:40:08 +0200

    create-gallery

---
+++ src/js/gallery.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  grabCursor: true,
+  loop: true,
+});
+
+const swiperBullets = document.querySelectorAll('.swiper-pagination-bullet');
+swiperBullets.forEach(bullet => {
+  bullet.setAttribute('style', 'background:#ffffff');
+});
