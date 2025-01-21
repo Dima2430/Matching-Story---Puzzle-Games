2d2408303488b46956ecfb0a7c4d0064e7f5c234
Dima2430 <dima.volkkom@gmail.com>
2025-01-21 21:52:01 +0100

    added hovers and clouds

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,8 @@ const menuIcon = document.querySelector('.menu-button');
 const backdrop = document.querySelector('.backdrop');
 const closeIcon = document.querySelector('.close-button');
 const listOfLinks = document.querySelector('.nav-list');
+const headerLinks = document.querySelectorAll('.nav-link');
+const footerLinks = document.querySelectorAll('.footer-link');
 menuIcon.addEventListener('click', () => {
   backdrop.classList.add('is-open');
 });
@@ -12,4 +14,30 @@ listOfLinks.addEventListener('click', e => {
   if (e.target.classList.contains('nav-link')) {
     backdrop.classList.remove('is-open');
   }
+});
+
+headerLinks.forEach(link => {
+  link.addEventListener('mousedown', () => {
+    headerLinks.forEach(item => item.classList.remove('active-link'));
+    link.classList.add('active-link');
+  });
+
+  link.addEventListener('mouseup', () => {
+    setTimeout(() => {
+      link.classList.remove('active-link');
+    }, 0);
+  });
+});
+
+footerLinks.forEach(link => {
+  link.addEventListener('mousedown', () => {
+    footerLinks.forEach(item => item.classList.remove('active-link'));
+    link.classList.add('active-link');
+  });
+
+  link.addEventListener('mouseup', () => {
+    setTimeout(() => {
+      link.classList.remove('active-link');
+    }, 0);
+  });
 });
